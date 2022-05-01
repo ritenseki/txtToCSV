909b2317f488692227501fc29047fc0c4a29bd2a
Ritenseki <ritenseki_p@outlook.com>
2022-05-01 10:11:58 +0800

    修bug

--- format.py
+++ format.py
@@ -1,3 +1,4 @@
+# coding = UTF-8
 import pandas as pd
 
 if __name__ == "__main__":
@@ -9,12 +10,15 @@ if __name__ == "__main__":
         text = []
         keys = name.lstrip('FileSource/')
         print(name)
+        keynum = 1
         for line in file.readlines():
             line = line.strip()
-
-            key.append(rf'key_{keys}')
+            key.append(rf'key_{keys}_line{keynum}')
             text.append(line)
+            keynum = keynum + 1
+
         paragraph = {'keys(csv文件索引)': key, 'text(csv文件索引)': text}
         data = pd.DataFrame(paragraph)
         formatdata = data
-        formatdata.to_csv(f'{name}.csv', index=False,)
+        print(data)
+        formatdata.to_csv(f'{name}.csv', index=False)
