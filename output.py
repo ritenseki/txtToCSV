57fe8609b79091cc9213c9a21c1b02152e473efd
Ritenseki <ritenseki_p@outlook.com>
2022-05-05 21:46:23 +0800

    增加了一个csv导出回txt的脚本

---
+++ output.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import pandas as pd
+import fnmatch
+
+if __name__ == "__main__":
+    print('csv 导出为 txt工具 for Paratranz, Copyright 2022 Ritenseki.')
+    print('Ritenseki on Github: https://github.com/Ritenseki')
+    # print('本工具免费提供。')
+    print('开始转换？')
+    inputting = input('[Y/n]')
+    if inputting == 'Y':
+        try:
+            sourcedirs = os.listdir('.\Translated')
+        except FileNotFoundError as error:
+            print('找不到Translated目录，是否新建？')
+            inp = input('[Y/n]')
+            if inp == 'Y':
+                os.makedirs('Translated')
+            else:
+                sys.exit()
+
+        else:
+            for f in sourcedirs:
+                if fnmatch.fnmatch(f, '*.csv'):
+                    filesource = rf'.\Translated/{f}'
+                    # print(filesource)
+                    filen = filesource.rstrip('.csv')
+                    filenam = filen.lstrip(f'{filesource}')
+                    filename = f'{filen}.txt'
+                    file = open(filename, encoding='UTF-8', mode='w')
+                    dataframe = pd.DataFrame.dropna(pd.read_csv(f'{filesource}', usecols=[1], engine='python'))
+                    print(dataframe.values)
+                    for line in dataframe.values:
+                        for text in line:
+                            file.write(text)
+                            file.write('\n')
+                    print('Done!')
+                    file.close()
+        finally:
+            sys.exit()
